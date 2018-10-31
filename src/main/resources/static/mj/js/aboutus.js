3a445a5c433c51d44ff1070c615d19f82b2a7f99
李楠楠 <nannan.li@tpadsz.cn>
2018-10-31 17:59:31 +0800

    标注细节修改

--- src/main/resources/static/mj/js/aboutus.js
+++ src/main/resources/static/mj/js/aboutus.js
@@ -7,6 +7,19 @@ $(function(){
     var end=$(".p_end").offset().top;
     var line=(end-start);
     var style='';
+    var width=$(window).width();
+    var id;
+    if(width>1170){
+        $(".cd-timeline-block").each(function () {
+           id = $(this).prop("id");
+            if (id%2!=0){
+            //左侧时间轴
+                $(this).children(".cd-timeline-content").children("h2").css(
+                    {"marginLeft":75+'%',"text-align":"right"});
+                $(this).children(".cd-timeline-content").children("p").css("text-align","right");
+            }
+        });
+    }
     if ($("style").hasClass("myStyle")){
         $(".myStyle").html('#cd-timeline::before{height: '+line+'px;}');
     }else {
@@ -17,21 +30,29 @@ $(function(){
     $(window).resize(function () {
         start=$(".p_start").offset().top;
         end=$(".p_end").offset().top;
+        width=$(window).width();
         line=((end-start));
         $(".myStyle").html('#cd-timeline::before{height: '+line+'px;}');
+        if(width>1170){
+            $(".cd-timeline-block").each(function () {
+                id = $(this).prop("id");
+                console.log("id:"+id);
+                if (id%2!=0){
+                    //左侧时间轴
+                    $(this).children(".cd-timeline-content").children("h2").css(
+                        {"marginLeft":75+'%',"text-align":"right"});
+                    $(this).children(".cd-timeline-content").children("p").css("text-align","right");
+                }
+            });
+        }else {
+            $(".cd-timeline-block").each(function () {
+                id = $(this).prop("id");
+                if (id%2!=0){
+                    //移除左侧时间轴效果
+                    $(this).children(".cd-timeline-content").children("h2").css({"marginLeft":"","text-align":""});
+                    $(this).children(".cd-timeline-content").children("p").css("text-align","");
+                }
+            });
+        }
     });
-
-    //返回顶部
-    // var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body'); //operaFix
-    // $(".totop").hide();
-    // $(".totop").click(function () {
-    //     $body.animate({scrollTop: 0});
-    // });
-    // window.onscroll = function () {
-    //     if ((document.documentElement.scrollTop + document.body.scrollTop) > 200) {
-    //         $(".totop").show();
-    //     } else {
-    //         $(".totop").hide();
-    //     }
-    // };
 });
